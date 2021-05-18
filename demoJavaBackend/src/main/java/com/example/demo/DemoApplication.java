903fe6bbbf7ed1f69e2d7c3e58fc5973305958a1
Y.S.Shih <yusen.shih@gmail.com>
2021-05-18 23:33:28 +0800

    modify for deploy to wildfly

--- demoJavaBackend/src/main/java/com/example/demo/DemoApplication.java
+++ demoJavaBackend/src/main/java/com/example/demo/DemoApplication.java
@@ -2,15 +2,27 @@
               package com.example.demo;
               import org.springframework.boot.SpringApplication;
               import org.springframework.boot.autoconfigure.SpringBootApplication;
-              import org.springframework.context.annotation.ComponentScan;       
+              import org.springframework.boot.builder.SpringApplicationBuilder;
+			  import org.springframework.context.annotation.ComponentScan;    
+              // Spring Boot 2.x
+              import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;   
              
               @SpringBootApplication
               @ComponentScan(basePackages={"com.example.demo"})            
-              public class DemoApplication {
+              public class DemoApplication extends SpringBootServletInitializer {
                 
                   
                   public static void main(String[] args) {
                     SpringApplication.run(DemoApplication.class, args);                
                   }     
+                  @Override
+                  protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+                      return application.sources(DemoApplication.class);
+                  }
+               
+                  private static Class<DemoApplication> applicationClass = DemoApplication.class;
+              
+              
+              
               }
             
--- demoJavaBackend/src/main/java/com/example/demo/controller/TutorialController.java
+++ demoJavaBackend/src/main/java/com/example/demo/controller/TutorialController.java
@@ -22,7 +22,7 @@ import org.springframework.web.bind.annotation.RestController;
 import com.example.demo.model.Tutorial;
 import com.example.demo.repository.TutorialRepository;
 
-@CrossOrigin(origins = "http://localhost:8081")
+@CrossOrigin(origins = "http://localhost:8080")
 @RestController
 @RequestMapping("/api")
 public class TutorialController {
